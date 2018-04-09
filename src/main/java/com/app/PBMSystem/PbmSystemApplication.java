8a1f7461d1000978c2bd98b35728ece8858990e2
Dawid Branicki <braniak94@gmail.com>
2018-04-09 13:35:56 +0200

    init project

---
+++ src/main/java/com/app/PBMSystem/PbmSystemApplication.java
@@ -0,0 +1,12 @@
+package com.app.PBMSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbmSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbmSystemApplication.class, args);
+	}
+}
