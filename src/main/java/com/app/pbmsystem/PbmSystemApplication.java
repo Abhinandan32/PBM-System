ee77747b6d6cb76e3b3e356e8f85641992a9fa90
Dawid Branicki <braniak94@gmail.com>
2018-04-13 22:57:50 +0200

    polaczenie z baza danych

--- src/main/java/com/app/pbmsystem/PbmSystemApplication.java
+++ src/main/java/com/app/pbmsystem/PbmSystemApplication.java
@@ -1,4 +1,4 @@
-package com.app.PBMSystem;
+package com.app.pbmsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/app/pbmsystem/PbmSystemApplicationTests.java
+++ src/test/java/com/app/pbmsystem/PbmSystemApplicationTests.java
@@ -1,4 +1,4 @@
-package com.app.PBMSystem;
+package com.app.pbmsystem;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
