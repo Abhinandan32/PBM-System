b80a06800ea1796fbe5027ff13fd7fd19e52e876
Dawid Branicki <braniak94@gmail.com>
2018-06-04 19:33:41 +0200

    security fix

--- src/main/java/com/app/pbmsystem/controller/UserController.java
+++ src/main/java/com/app/pbmsystem/controller/UserController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -30,7 +31,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @RequestMapping(value = "/login")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     public Principal user(Principal principal) {
         LOGGER.info("user logged " + principal);
         return principal;
